2df3f04da79f54d190ffe8ab5fec856bf1de933d
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2018-03-29 07:09:12 +0300

    added GSS

---
+++ FormalLang5sem/FormalLang5sem/Entities/GraphStructuredStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormalLang5sem.Entities
+{
+    class GraphStructuredStack
+    {
+        private List<(string, int)> _vertices;
+        private Dictionary<int, List<(int, int)>> _edges;
+
+
+        public void AddVertex(string nonterminal, int positionInGraph)
+        {
+            var tuple = (nonterminal, positionInGraph);
+
+            if (!_vertices.Contains(tuple))
+            {
+                _vertices.Add(tuple);
+            }
+        }
+
+
+        public int GetPositionOfVertex(string nonterminal, int positionInGraph) 
+            => _vertices.IndexOf((nonterminal, positionInGraph));
+
+
+        public void AddEdge(int vertexPosition1, int positionInGrammar, int vertexPosition2)
+        {
+            if (!_edges.ContainsKey(vertexPosition1))
+            {
+                _edges.Add(vertexPosition1, new List<(int, int)>());
+            }
+
+            var tuple = (positionInGrammar, vertexPosition2);
+            if (!_edges[vertexPosition1].Contains(tuple))
+            {
+                _edges[vertexPosition1].Add(tuple);
+            }
+        }
+    }
+}
