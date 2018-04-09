3e9d5d24702e83f459cb77a9cbe7bc656a691757
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2018-04-10 02:54:46 +0300

    added CartesianProduct

---
+++ FormalLang5sem/FormalLang5sem/Entities/CartesianProduct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FormalLang5sem.Entities
+{
+    class CartesianProduct<T>
+    {
+        private List<string>[,] _table;
+        private Dictionary<int, T> _valueByIndex1;  //  in rows
+        private Dictionary<T, int> _indexByValue1;  //  in rows
+        private Dictionary<int, T> _valueByIndex2;  //  in columns
+        private Dictionary<T, int> _indexByValue2;  //  in columns
+
+
+        public int RowsCount;
+        public int ColumnsCount;
+
+
+        public CartesianProduct(ISet<T> set1, ISet<T> set2)
+        {
+            RowsCount = set1.Count;
+            ColumnsCount = set2.Count;
+            _table = new List<string>[RowsCount, ColumnsCount];
+
+            for (int i = 0; i < RowsCount; i++)
+            {
+                for (int j = 0; j < ColumnsCount; j++)
+                {
+                    _table[i, j] = new List<string>();
+                }
+            }
+
+            IndexesMatching(set1, set2);
+        }
+
+
+        private void IndexesMatching(ISet<T> set1, ISet<T> set2)
+        {
+            int k = 0;
+            _valueByIndex1 = set1.ToDictionary(t => k++);
+            _indexByValue1 = _valueByIndex1.ToDictionary(t => t.Value, t => t.Key);
+
+            k = 0;
+            _valueByIndex2 = set2.ToDictionary(t => k++);
+            _indexByValue2 = _valueByIndex2.ToDictionary(t => t.Value, t => t.Key);
+        }
+    }
+}
