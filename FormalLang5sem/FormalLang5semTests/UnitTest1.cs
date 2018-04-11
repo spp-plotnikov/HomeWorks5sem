624e508f093bfb3bebc77fd2b22edeaeccbfa9ea
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2018-04-11 07:17:41 +0300

    added project for unit tests

---
+++ FormalLang5sem/FormalLang5semTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormalLang5semTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
