9007a6a18de5181da1cdb97f7a3049d37daa2d94
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2018-04-11 10:12:15 +0300

    improved test

--- FormalLang5sem/FormalLang5sem/Tests.cs
+++ FormalLang5sem/FormalLang5sem/Tests.cs
@@ -14,14 +14,16 @@ namespace FormalLang5sem
     {
         public void GLLSolverTest()
         {
-            string allResults = "";
             var solver = new GLLSolver();
-            foreach (var grammarFile in _grammars)
+            foreach (var grammarName in _grammars)
             {
-                foreach (var graphFile in _automata)
+                foreach (var graphName in _automata)
                 {
-                    var graphDot = File.ReadAllText("Resources\\Automata\\" + graphFile, Encoding.Default);
-                    var grammarDot = File.ReadAllText("Resources\\Grammars\\" + grammarFile, Encoding.Default);
+                    var graphFile = "Resources\\Automata\\" + graphName + ".dot";
+                    var grammarFile = "Resources\\Grammars\\" + grammarName + ".dot";
+
+                    var graphDot = File.ReadAllText(graphFile, Encoding.Default);
+                    var grammarDot = File.ReadAllText(grammarFile, Encoding.Default);
 
                     var graph = new Graph(graphDot);
                     var grammar = Grammar.FromDot(grammarDot);
@@ -29,36 +31,74 @@ namespace FormalLang5sem
                     var result = solver.Solve(graph, grammar);
                     var count = result.Split('S').Length - 1;  // count of triplets (i,S,j)
 
-                    allResults += graphFile + ' ' + grammarFile + ' '
-                               + count.ToString() + Environment.NewLine;
+                    Console.Write("Automation: " + graphName + "; Grammar: " + grammarName);
+                    Console.Write("; Result: " + count.ToString());
+                    result = _correctResults[graphName + grammarName] == count ? " test passed" : " test failed";
+                    Console.Write(result + Environment.NewLine);
                 }
             }
-
-            File.WriteAllText("GLLSolver_results.txt", allResults);
         }
 
 
         private static string[] _automata =
         {
-            "skos.dot",
-            "generations.dot",
-            "travel.dot",
-            "univ-bench.dot",
-            "atom-primitive.dot",
-            "biomedical-mesure-primitive.dot",
-            "foaf.dot",
-            "people_pets.dot",
-            "funding.dot",
-            "wine.dot",
-            "pizza.dot"
+            "skos",
+            "generations",
+            "travel",
+            "univ-bench",
+            "atom-primitive",
+            "biomedical-mesure-primitive",
+            "foaf",
+            "people_pets",
+            "funding",
+            "wine",
+            "pizza"
         };
 
 
         private static string[] _grammars =
         {
-            "Q1.dot",
-            "Q2.dot",
-            "Q3.dot"
+            "Q1",
+            "Q2",
+            "Q3"
+        };
+
+
+        private static Dictionary<string, int> _correctResults = new Dictionary<string, int>()
+        {
+            { "skosQ1", 810 },
+            { "generationsQ1", 2164 },
+            { "travelQ1", 2499 },
+            { "univ-benchQ1", 2540 },
+            { "atom-primitiveQ1", 15454 },
+            { "biomedical-mesure-primitiveQ1", 15156 },
+            { "foafQ1", 4118 },
+            { "people_petsQ1", 9472 },
+            { "fundingQ1", 17634 },
+            { "wineQ1", 66572 },
+            { "pizzaQ1", 56195 },
+            { "skosQ2", 1 },
+            { "generationsQ2", 0 },
+            { "travelQ2", 63 },
+            { "univ-benchQ2", 81 },
+            { "atom-primitiveQ2", 122 },
+            { "biomedical-mesure-primitiveQ2", 2871 },
+            { "foafQ2", 10 },
+            { "people_petsQ2", 37 },
+            { "fundingQ2", 1158 },
+            { "wineQ2", 133 },
+            { "pizzaQ2", 1262 },
+            { "skosQ3", 32 },
+            { "generationsQ3", 19 },
+            { "travelQ3", 31 },
+            { "univ-benchQ3", 12 },
+            { "atom-primitiveQ3", 3 },
+            { "biomedical-mesure-primitiveQ3", 0 },
+            { "foafQ3", 46 },
+            { "people_petsQ3", 36 },
+            { "fundingQ3", 18 },
+            { "wineQ3", 1215 },
+            { "pizzaQ3", 9520 }
         };
     }
 }
